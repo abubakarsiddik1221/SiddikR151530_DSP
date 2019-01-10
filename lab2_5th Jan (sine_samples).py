75fe66b9e42a81851064bc8918aec795bb004860
abubakarsiddik1221 <46397963+abubakarsiddik1221@users.noreply.github.com>
2019-01-10 19:10:49 +0530

    Delete lab2_5th Jan (sine_samples).py

--- lab2_5th Jan (sine_samples).py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as m
-F=5
-Fs=10
-n=m.arange(0,10,0.25)
-A=m.sin((2*m.pi*F*n)/Fs)
-plt.xlabel("------>time")
-plt.ylabel("------>Amplitude")
-plt.stem(n,A)
